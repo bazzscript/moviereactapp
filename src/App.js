9766b0c98d377461d8ad4eaf8490d5370668714a
BEZALEEL NWABIA <bezaleelnwabia@gmail.com>
2024-07-28 23:37:23 +0100

    Intiial Commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies("Lantern");
   }, []);
 
   const searchMovies = async (title) => {
